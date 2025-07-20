7fce4147300ef14d82820d4d17b3330c8dc1f30b
Adityakalburgi <aditya_kalburgi@cargill.com>
2025-07-20 20:59:21 +0530

    initial

---
+++ main.tf
@@ -0,0 +1,15 @@
+# dummy-terraform/main.tf
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.4"
+    }
+  }
+}
+
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "echo Hello, Terraform!, and its value is ${dummy_variable}"
+  }
+}
